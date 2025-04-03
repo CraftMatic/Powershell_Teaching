717cd05e9970e638c8e8249dbdf4d7173fb3472a
CraftMatic <alexander.craft.23@gmail.com>
2025-04-02 20:47:00 -0400

    Add files via upload
    Adding Folder and file creation script.

---
+++ FolderandContentCreationExample.ps1
@@ -0,0 +1,17 @@
+﻿# Create a folder named "ENC"
+$folderpath = "C:\ENC"
+New-Item -ItemType Directory -Path $folderpath
+Write-Output "$folderpath was created successfully" 
+
+
+# create a test document in the "ENC" folder
+$filename = "Testbed.txt"
+$testfilepath = "$folderpath\$filename"
+new-item -ItemType File -Path $testfilepath
+Write-Output "$filename was created successfully"
+
+
+# Prompt the user for what content to add to the file we are creating
+$content = Read-Host "Enter the content you would like to add to the document"
+Set-Content -Path $testfilepath -Value $content
+Write-Output "Content has been added to the test document"
